aac339b0a85a2f54670bac5b7f09aa41a280b9f5
Dominik Keisz <keisz.d@gmail.com>
2021-08-30 16:20:26 +0200

    Created temaplte and added http.Handler

--- cmd/cyoaweb/main.go
+++ cmd/cyoaweb/main.go
@@ -3,15 +3,18 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
+	"net/http"
 	"os"
 
 	"github.com/dominikkeisz/cyoa"
 )
 
 func main() {
+	port := flag.Int("port", 3000, "the port to start the CYOA web application on")
 	filename := flag.String("file", "gopher.json", "the JSON file with the CYOA story")
 	flag.Parse()
-	fmt.Printf("Using the story in %s", *filename)
+	fmt.Printf("Using the story in %s\n", *filename)
 
 	f, err := os.Open(*filename)
 	if err != nil {
@@ -23,5 +26,7 @@ func main() {
 		panic(err)
 	}
 
-	fmt.Printf("%+v", story)
+	h := cyoa.NewHandler(story)
+	fmt.Printf("Starting the server on port: %d\n", *port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), h))
 }
--- story.go
+++ story.go
@@ -3,7 +3,105 @@ package cyoa
 import (
 	"encoding/json"
 	"io"
+	"net/http"
+	"text/template"
 )
+
+type Story map[string]Chapter
+
+type Chapter struct {
+	Title      string   `json:"title"`
+	Paragraphs []string `json:"story"`
+	Options    []Option `json:"options"`
+}
+
+type Option struct {
+	Text    string `json:"text"`
+	Chapter string `json:"arc"`
+}
+
+type handler struct {
+	s Story
+}
+
+var storyTmpl = `
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Choose Your Own Adventure</title>
+  </head>
+  <body>
+    <section class="page">
+      <h1>{{.Title}}</h1>
+      {{range .Paragraphs}}
+        <p>{{.}}</p>
+      {{end}}
+      <ul>
+      {{range .Options}}
+        <li><a href="/{{.Chapter}}">{{.Text}}</a></li>
+      {{end}}
+      </ul>
+    </section>
+    <style>
+      body {
+        font-family: helvetica, arial;
+      }
+      h1 {
+        text-align:center;
+        position:relative;
+      }
+      .page {
+        width: 80%;
+        max-width: 500px;
+        margin: auto;
+        margin-top: 40px;
+        margin-bottom: 40px;
+        padding: 80px;
+        background: #FCF6FC;
+        border: 1px solid #eee;
+        box-shadow: 0 10px 6px -6px #797;
+      }
+      ul {
+        border-top: 1px dotted #ccc;
+        padding: 10px 0 0 0;
+        -webkit-padding-start: 0;
+      }
+      li {
+        padding-top: 10px;
+      }
+      a,
+      a:visited {
+        text-decoration: underline;
+        color: #555;
+      }
+      a:active,
+      a:hover {
+        color: #222;
+      }
+      p {
+        text-indent: 1em;
+      }
+    </style>
+  </body>
+</html>`
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.New("").Parse(storyTmpl))
+}
+
+func NewHandler(s Story) http.Handler {
+	return handler{s}
+}
+
+func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	err := tpl.Execute(w, h.s["intro"])
+	if err != nil {
+		panic(err)
+	}
+}
 
 func JsonStory(r io.Reader) (Story, error) {
 	d := json.NewDecoder(r)
@@ -13,17 +111,4 @@ func JsonStory(r io.Reader) (Story, error) {
 	}
 
 	return story, nil
-}
-
-type Story map[string]Chapter
-
-type Chapter struct {
-	Title      string   `json:"title"`
-	Paragprahs []string `json:"story"`
-	Options    []Option `json:"options"`
-}
-
-type Option struct {
-	Text    string `json:"text"`
-	Chapter string `json:"arc"`
 }
